62272cd82acea69141c74a792234b4dae6f50d3a
Michael Burman <miburman@redhat.com>
2014-12-09 14:01:32 +0200

    Add error reply

--- routing/query.go
+++ routing/query.go
@@ -1,6 +1,7 @@
 package routing
 
 import (
+	"fmt"
 	"github.com/gorilla/mux"
 	"net/http"
 )
@@ -8,13 +9,20 @@ import (
 func QlmQuery(w http.ResponseWriter, r *http.Request, db Datastore) {
 	vars := mux.Vars(r)
 	w.Header().Set("Content-Type", "application/xml")
+	var resp []byte
+	var err error
 	if node, found := vars["node"]; found {
 		// Request node details
-		resp, _ := KeyList(node, db)
-		w.Write(resp)
+		resp, err = KeyList(node, db)
 	} else {
 		// Request list of available nodes
-		resp, _ := NodeList(db)
-		w.Write(resp)
+		resp, err = NodeList(db)
 	}
+	if err != nil {
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(http.StatusInternalServerError)
+		fmt.Fprintf(w, "Could not fetch requested info %s", err)
+		return
+	}
+	w.Write(resp)
 }
